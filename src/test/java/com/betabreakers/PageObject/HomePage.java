5f4e5f37afbd6902808a9ca0d0c7848b297008ce
gallenjohnson <gaerdel@gmail.com>
2015-12-07 16:34:26 -0800

    Minor grammar edit in line 10.

--- src/test/java/com/betabreakers/PageObject/HomePage.java
+++ src/test/java/com/betabreakers/PageObject/HomePage.java
@@ -7,9 +7,10 @@ public class HomePage extends BasePage{
 
 	public HomePage(WebDriver driver) {
 		super(driver);
-		Assert.assertTrue("HomePage Assertion Failed", driver
-														.getCurrentUrl()
-														.equals("http://www.betabreakers.com"));
+		Assert.assertTrue("HomePage Assertion Failed",
+							driver
+							.getCurrentUrl()
+							.equals("http://www.betabreakers.com"));
 	}
 
 }
