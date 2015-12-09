7140a148e71c5c7a9e047ddcf6e10397a65c32ff
gallenjohnson <gaerdel@gmail.com>
2015-12-09 08:23:06 -0800

    Added access modifier to homePage variable in BasePageStepDefs.

--- src/test/java/com/betabreakers/BB_Website_Cuke_Automation/BasePageStepDefs.java
+++ src/test/java/com/betabreakers/BB_Website_Cuke_Automation/BasePageStepDefs.java
@@ -6,15 +6,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import com.betabreakers.PageObject.HomePage;
 
 public class BasePageStepDefs {
-	HomePage homePage;
+	protected static WebDriver driver;
+	protected HomePage homePage;
 
 
 	BasePageStepDefs(){
 		getDriver();
 		homePage = new HomePage(driver);
 	}
-
-	protected static WebDriver driver;
 
 	protected WebDriver getDriver() {
 		if(driver == null) {
