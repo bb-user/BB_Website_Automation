d0253127395c3eeb63d43f1fc26934f3475df0aa
gallenjohnson <gaerdel@gmail.com>
2015-12-09 08:48:07 -0800

    Removed commented out code in HomePage.

--- src/test/java/com/betabreakers/PageObject/HomePage.java
+++ src/test/java/com/betabreakers/PageObject/HomePage.java
@@ -6,10 +6,6 @@ public class HomePage extends BasePage{
 
 	public HomePage(WebDriver driver) {
 		super(driver);
-//		Assert.assertTrue("HomePage Assertion Failed",
-//							driver
-//							.getCurrentUrl()
-//							.equals("http://www.betabreakers.com"));
 	}
 
 }
