38bc3d00085f74ff001d2fc5f6e310a319b53b5d
gallenjohnson <gaerdel@gmail.com>
2015-12-10 10:20:36 -0800

    Declared objects for all of the page classes.

--- src/test/java/com/betabreakers/BB_Website_Cuke_Automation/BasePageStepDefs.java
+++ src/test/java/com/betabreakers/BB_Website_Cuke_Automation/BasePageStepDefs.java
@@ -3,11 +3,23 @@ package com.betabreakers.BB_Website_Cuke_Automation;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.betabreakers.PageObject.BlogPage;
+import com.betabreakers.PageObject.CompanyPage;
+import com.betabreakers.PageObject.ContactPage;
 import com.betabreakers.PageObject.HomePage;
+import com.betabreakers.PageObject.ServicesPage;
+import com.betabreakers.PageObject.TheLabsPage;
+import com.betabreakers.PageObject.WhyTestPage;
 
 public class BasePageStepDefs {
 	protected static WebDriver driver;
 	protected HomePage homePage;
+	protected ServicesPage servicesPage;
+	protected WhyTestPage whyTestPage;
+	protected TheLabsPage theLabsPage;
+	protected CompanyPage companyPage;
+	protected ContactPage contactPage;
+	protected BlogPage blogPage;
 
 	BasePageStepDefs(){
 		getDriver();
