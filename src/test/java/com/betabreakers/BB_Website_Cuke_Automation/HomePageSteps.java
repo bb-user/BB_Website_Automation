cf554a525bf2bd80ed84e0c7d5b9fc478cf43d67
Trammel <tramstheman@gmail.com>
2015-12-11 11:54:11 -0800

    homepage steps

---
+++ src/test/java/com/betabreakers/BB_Website_Cuke_Automation/HomePageSteps.java
@@ -0,0 +1,87 @@
+package com.betabreakers.BB_Website_Cuke_Automation;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.betabreakers.PageObject.HomePage;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HomePageSteps 
+{
+	HomePage homePage = new HomePage(new FirefoxDriver());
+	
+	@Given("^I am on the home page$")
+	public void i_am_on_the_home_page() throws Throwable 
+	{
+	    System.out.println("this is where verficiation of home page with exception handling is done");
+	}
+
+	
+	
+	@When("^I click on Whats Your Next Project$")
+	public void i_click_on_Whats_Your_Next_Project() throws Throwable 
+	{
+	    
+
+	}
+
+	@Then("^I should be on the Services Page$")
+	public void i_should_be_on_the_Services_Page() throws Throwable 
+	{
+	    
+
+	}
+
+	@When("^I click on a client$")
+	public void i_click_on_a_client() throws Throwable 
+	{
+	    
+
+	}
+
+	@Then("^I should be on that clients referal page$")
+	public void i_should_be_on_that_clients_referal_page() throws Throwable 
+	{
+	  
+
+	}
+
+	@When("^I click a services link$")
+	public void i_click_a_services_link() throws Throwable
+	{
+	        
+	}
+
+	@Then("^I should be on that services page$")
+	public void i_should_be_on_that_services_page() throws Throwable 
+	{
+	    
+	}
+
+	@When("^I click Follow Our Plog$")
+	public void i_click_Follow_Our_Plog() throws Throwable 
+	{
+	   
+	}
+
+	@Then("^I should be on the first page of the block$")
+	public void i_should_be_on_the_first_page_of_the_block() throws Throwable 
+	{
+	    
+	}
+
+	@When("^I click a blog link$")
+	public void i_click_a_blog_link() throws Throwable
+	{
+	    
+	}
+
+	@Then("^I should be on that blogs page$")
+	public void i_should_be_on_that_blogs_page() throws Throwable 
+	{
+	    
+	}
+
+}
