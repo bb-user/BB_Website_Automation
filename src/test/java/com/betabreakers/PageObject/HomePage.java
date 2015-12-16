8a3d2162eef1f2534b4364501c3862e36daec9d9
gallenjohnson <gaerdel@gmail.com>
2015-12-16 14:33:31 -0800

    Added a getClient method with a switch case to return the value needed to point the clickOnPreviousClient method to the correct CSS selector.

--- src/test/java/com/betabreakers/PageObject/HomePage.java
+++ src/test/java/com/betabreakers/PageObject/HomePage.java
@@ -3,24 +3,42 @@ package com.betabreakers.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
 
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
 
 	public ServicesPage clickWhatsYourNextProject() {
-		driver.findElement(By.cssSelector("#slidewrap > div > div > a")).click();
+		driver.findElement(By.cssSelector("#slidewrap > div > div > a"))
+				.click();
 		return new ServicesPage(driver);
 	}
 
-	public TestimonialsPage clickOnPreviousClient(String client) {
-		driver.findElement(By.cssSelector
-								("#logoblock > div:nth-child(" + client + ") > a > img"))
-								.click();
-		return new TestimonialsPage(driver);
+	private static String getClient(String client) {
+		switch (client) {
+		case "cisco":
+			return "2";
+		case "oracle":
+			return "3";
+		case "scripps":
+			return "4";
+		case "leapfrog":
+			return "5";
+		case "snap on":
+			return "6";
+		case "trek":
+			return "7";
+		default:
+			return " ";
+		}
 	}
 
-
+	public TestimonialsPage clickOnPreviousClient(String client) {
+		driver.findElement(
+				By.cssSelector("#logoblock > div:nth-child(" + getClient(client)
+						+ ") > a > img")).click();
+		return new TestimonialsPage(driver);
+	}
 
 }
